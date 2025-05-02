449940b389be7e51d8f09e56ac98e5bd2c6e78da
SDA student <aldomecani20@gmail.com>
2025-05-02 17:26:56 +0200

    SecondComit

--- vite-project/src/componentes/products/ProductList.tsx
+++ vite-project/src/componentes/products/ProductList.tsx
@@ -83,6 +83,7 @@ export default function ProductList() {
             description={product.description}
             price={product.price}
             tags={product.tags}
+            id={""}
           />
         ))
       ) : (
